d572ffd429b1f9e104a01636cab78bfd8032378d
kagami <147567573+HarukaKagami@users.noreply.github.com>
2023-10-12 02:29:43 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -90,7 +90,7 @@
 /**
  * 分割代入
  */
-// const myProfile = {
+//const myProfile = {
 //        name: "aaa",
 //        age:28,
 // };
@@ -102,8 +102,8 @@
 // console.log(message2);
 
 // const myProfile = ['aaa', 28];
-// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
-// console.log(message3);
+//const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+//console.log(message3);
 
 // const [name, age] = myProfile;
 // const message4 = `名前は${name}です。年齢は${age}歳です。`;
@@ -152,3 +152,74 @@
 // console.log(arr8);
 // console.log(arr4);
 
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+const nameArr = ["田中", "山田", "aaa"];
+// for (let index = 0; index < nameArr.length; index++) {
+//     console.log(`${index + 1}番目は${nameArr[index]}です。`);
+// }
+
+// const nameArr2 = nameArr.map((name) => {
+//     return name;
+// });
+// console.log(nameArr2);
+
+// nameArr.map((name, index) => console.log(`${name}です`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//     return num % 2 === 0;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "aaa"){
+//     return name;
+//   }  else{
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+//ある条件 ? 条件がtrueのとき：条件がfalseの時
+// const val1 = 1 < 0 ? `trueです` : `falseです`;
+// console.log(val1);
+
+// const num = "1300";
+// console.log(num.toLocaleString());
+
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//     return num1 + num2 > 100 ? '100を超えています！' : '許容範囲内です';
+// }
+// console.log(checkSum(50, 40));
+
+/**
+ * 論理演算子の本当の意味を知ろう　&& ||
+ */
+// const flag1 = true;
+// const flag2 = false;
+
+// if(flag1 || flag2){
+//     console.log("1か2はtrueになります");
+// }
+// if(flag1 && flag2){
+//     console.log("1も2もtrueになります");
+// }
+
+// || は左側がfalseなら右側を返す
+// const num = 100;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// // && は左側がtrueなら右側を返す
+// const num2 = null;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
